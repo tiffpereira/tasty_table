7a1f71759caee81e3db28d60bdb8aa3ba0c22fb0
tiffpereira <tiffanympereira@gmail.com>
2022-03-08 09:52:25 -0500

    Added an additional button with a randomizer function

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const button = document.querySelector('#search')
 const searchInput = document.querySelector('#search-input')
+const randomButton = document.querySelector('#random')
 
+//Button 1 
 button.addEventListener("click", async () => {
     const recipe = searchInput.value 
     const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipe}`)
 
     console.log(response)
+
+    const renderList = response.data.meals 
+})
+
+//Button 2
+randomButton.addEventListener("click", async () => {
+    const recipe = searchInput.value 
+    const response = await axios.get(`https:www.themealdb.com/api/json/v1/1/random.php`)
+
+    console.log(response)
+
+    const renderList = response.data.meals
 })
 
