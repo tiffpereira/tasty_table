595b9e689febdf671a1d33dd00ffc8f7dd7a52a5
Tiffany <tiffanympereira@gmail.com>
2023-06-17 13:07:32 -0400

    configuring card info

--- mealList.js
+++ mealList.js
@@ -4,9 +4,12 @@ let mealData = JSON.parse(sessionStorage.getItem('mealData'));
 // Now you can use mealData in this file
 console.log(mealData.meals);
 
-const mealsList = mealData.data 
+const mealInfo = document.querySelector('.meal-card')
 
-mealsList.map((meal) => {
-    
+const mealsList = mealData.meals 
 
+mealsList.forEach((meal) => {
+    const mealTitle = document.createElement('h3')
+    mealTitle.innerHTML = `${meal.strMeal}`
+    mealInfo.append(mealTitle)
 })
